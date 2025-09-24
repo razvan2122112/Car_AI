70273308e5e3ce5284ca8ead0b1881683c0c57fe
razvan2122112 <razvanrusu2017@gmail.com>
2025-09-24 20:32:56 +0200

    Create index.js

---
+++ public/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CarAI from './driver-ai-assistant';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<CarAI />);
